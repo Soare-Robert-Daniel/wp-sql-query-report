6db851546f25dcc38173bcca5a895f2f29190994
Soare Robert-Daniel <soare.robert.daniel@protonmail.com>
2025-11-20 18:00:53 +0200

    init plugin

---
+++ sql-analyzer.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Sql Analyzer
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     sql-analyzer
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Sql_Analyzer
+ */
+
+// Your code starts here.
